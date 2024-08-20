09c5d595b2284e5c9c5f4321a4c6953cbca2158e
sandeep kumar <er.targetsandeepkumar@gmail.com>
2024-08-20 18:34:57 +0530

    new changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='footer' >
+      <div className='Social-media' style={{display:'flex',alignItems:'center',gap:'20px',justifyContent:'space-around'}}>
+        <ul style={{display:"flex",gap:"30px",listStyle:"none"}}>
+          <li>fb</li>
+          <li>yt</li>
+          <li>ig</li>
+        </ul>
+        <div>Logo</div>
+        <div>Contact</div>
+      </div>
+  Footer  
+  hello  
     </div>
   )
 }
