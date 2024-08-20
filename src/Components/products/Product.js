17cbb6c561fcc24ed3474ca63bb02e8a3f4a0359
sandeep-coder915 <er.targetsandeepkumar@gmail.com>
2024-08-21 00:03:10 +0530

    new chN

--- src/Components/products/Product.js
+++ src/Components/products/Product.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import img1  from './Assets/exe.png'
+import img2  from './Assets/Untitled design (66).png'
 
 const Product = () => {
   return (
-    <>  <h1 style={{textAlign:"center"}}>Explore Our Products</h1>
+    <>  <div style={{display:"flex",flexDirection:"column",alignItems:'center'}}><div><h1 style={{textAlign:"center"}}>Explore Our Products</h1></div>
     <div className='Container1'>
       
     <div className='row'>
         <div className='col-md-4'>
             <div className='card'>
                 <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
                 <div className='card-body'>
                     <h5 className='card-title'>Thriphala Powder </h5>
                     <p className='card-text'>Welcome To exotic veda  l</p>
@@ -22,6 +24,7 @@ const Product = () => {
         <div className='col-md-4'>
             <div className='card'>
                 <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
                 <div className='card-body'>
                     <h5 className='card-title'>Thriphala Powder </h5>
                     <p className='card-text'>Welcome To exotic veda  l</p>
@@ -34,6 +37,7 @@ const Product = () => {
         <div className='col-md-4'>
             <div className='card'>
                 <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
                 <div className='card-body'>
                     <h5 className='card-title'>Thriphala Powder </h5>
                     <p className='card-text'>Welcome To exotic veda  l</p>
@@ -46,6 +50,7 @@ const Product = () => {
         <div className='col-md-4'>
             <div className='card'>
                 <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
                 <div className='card-body'>
                     <h5 className='card-title'>Thriphala Powder </h5>
                     <p className='card-text'>Welcome To exotic veda  l</p>
@@ -61,30 +66,7 @@ const Product = () => {
         <div className='col-md-4'>
             <div className='card'>
                 <img src={img1} alt='img'></img>
-                <div className='card-body'>
-                    <h5 className='card-title'>Thriphala Powder </h5>
-                    <p className='card-text'>Welcome To exotic veda  l</p>
-                    <p className='card-text'><small className='text-muted'>Last updated 3 mins ago</small></p>
-                    <button className='btn btn-primary'>Buy Now</button>
-                </div>
-    
-            </div>
-            </div>
-        <div className='col-md-4'>
-            <div className='card'>
-                <img src={img1} alt='img'></img>
-                <div className='card-body'>
-                    <h5 className='card-title'>Thriphala Powder </h5>
-                    <p className='card-text'>Welcome To exotic veda  l</p>
-                    <p className='card-text'><small className='text-muted'>Last updated 3 mins ago</small></p>
-                    <button className='btn btn-primary'>Buy Now</button>
-                </div>
-    
-            </div>
-            </div>
-        <div className='col-md-4'>
-            <div className='card'>
-                <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
                 <div className='card-body'>
                     <h5 className='card-title'>Thriphala Powder </h5>
                     <p className='card-text'>Welcome To exotic veda  l</p>
@@ -97,11 +79,38 @@ const Product = () => {
         <div className='col-md-4'>
             <div className='card'>
                 <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
                 <div className='card-body'>
                     <h5 className='card-title'>Thriphala Powder </h5>
                     <p className='card-text'>Welcome To exotic veda  l</p>
                     <p className='card-text'><small className='text-muted'>Last updated 3 mins ago</small></p>
-                    <button className='btn btn-primary'><span>Buy Now</span></button>
+                    <button className='btn btn-primary'>Buy Now</button>
+                </div>
+    
+            </div>
+            </div>
+        <div className='col-md-4'>
+            <div className='card'>
+                <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
+                <div className='card-body'>
+                    <h5 className='card-title'>Thriphala Powder </h5>
+                    <p className='card-text'>Welcome To exotic veda  l</p>
+                    <p className='card-text'><small className='text-muted'>Last updated 3 mins ago</small></p>
+                    <button className='btn btn-primary'>Buy Now</button>
+                </div>
+    
+            </div>
+            </div>
+        <div className='col-md-4'>
+            <div className='card'>
+                <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
+                <div className='card-body'>
+                    <h5 className='card-title'>Thriphala Powder </h5>
+                    <p className='card-text'>Welcome To exotic veda  l</p>
+                    <p className='card-text'><small className='text-muted'>Last updated 3 mins ago</small></p>
+                    <button className='btn btn-primary'>Buy Now</button>
                 </div>
     
             </div>
@@ -112,6 +121,20 @@ const Product = () => {
         <div className='col-md-4'>
             <div className='card'>
                 <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
+                <div className='card-body'>
+                    <h5 className='card-title'>Thriphala Powder </h5>
+                    <p className='card-text'>Welcome To exotic veda  l</p>
+                    <p className='card-text'><small className='text-muted'>Last updated 3 mins ago</small></p>
+                    <a href='https://www.revaais.shop/collections/exotic-veda'><button className='btn btn-primary'>Buy Now</button></a>
+                </div>
+    
+            </div>
+            </div>
+        <div className='col-md-4'>
+            <div className='card'>
+                <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
                 <div className='card-body'>
                     <h5 className='card-title'>Thriphala Powder </h5>
                     <p className='card-text'>Welcome To exotic veda  l</p>
@@ -124,6 +147,7 @@ const Product = () => {
         <div className='col-md-4'>
             <div className='card'>
                 <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
                 <div className='card-body'>
                     <h5 className='card-title'>Thriphala Powder </h5>
                     <p className='card-text'>Welcome To exotic veda  l</p>
@@ -136,18 +160,7 @@ const Product = () => {
         <div className='col-md-4'>
             <div className='card'>
                 <img src={img1} alt='img'></img>
-                <div className='card-body'>
-                    <h5 className='card-title'>Thriphala Powder </h5>
-                    <p className='card-text'>Welcome To exotic veda  l</p>
-                    <p className='card-text'><small className='text-muted'>Last updated 3 mins ago</small></p>
-                    <button className='btn btn-primary'>Buy Now</button>
-                </div>
-    
-            </div>
-            </div>
-        <div className='col-md-4'>
-            <div className='card'>
-                <img src={img1} alt='img'></img>
+                <img src={img2} alt='img'></img>
                 <div className='card-body'>
                     <h5 className='card-title'>Thriphala Powder </h5>
                     <p className='card-text'>Welcome To exotic veda  l</p>
@@ -159,7 +172,8 @@ const Product = () => {
             </div>
   
             </div>
-    
+ 
+            </div>
             </div></>
   )
 }
